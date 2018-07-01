7e19dbe5df8df78c182b3153a33b6692d6d2b571
Jared De Blander <jared@quantumcodeworks.com>
2018-07-01 01:30:26 -0400

    Update NEStroller.cpp

--- NEStroller.cpp
+++ NEStroller.cpp
@@ -8,8 +8,11 @@
 #include "Arduino.h"
 #include "NEStroller.h"
 
-#define TIMING_DELAY    4
+#define TIMING_DELAY    12
 
+// -----------------------------------------------------------------------------
+// NEStroller
+// -----------------------------------------------------------------------------
 NEStroller::NEStroller() {
 
 }
@@ -47,5 +50,54 @@ bool NEStroller::getButtonPressed(int button_requested) {
 }
 
 byte NEStroller::getButtons() {
+  return _button_states;
+}
+
+// -----------------------------------------------------------------------------
+// SNEStroller
+// -----------------------------------------------------------------------------
+
+SNEStroller::SNEStroller() {
+
+}
+
+void SNEStroller::begin(int pinClock, int pinLatch, int pinDataIn) {
+  _pin_clock = pinClock;
+  _pin_latch = pinLatch;
+  _pin_data_in = pinDataIn;
+
+  pinMode(_pin_clock , OUTPUT);
+  pinMode(_pin_latch , OUTPUT);
+  pinMode(_pin_data_in , INPUT);
+}
+
+void SNEStroller::updateButtons() {
+  // reset button states
+  _button_states = 0;
+
+  // Pulse the latch (aka strobe pin)
+  digitalWrite(_pin_latch, HIGH);
+  delayMicroseconds(TIMING_DELAY *10);
+  digitalWrite(_pin_latch, LOW);
+
+  // Serial.print("[begin]");
+  for (int buttons = 15; buttons >= 0; buttons--) {
+    auto data_in = digitalRead(_pin_data_in);
+    // Serial.print(data_in);
+    // Serial.print("  ");
+    _button_states |= digitalRead(_pin_data_in) << buttons;
+    delayMicroseconds(TIMING_DELAY);
+    digitalWrite(_pin_clock, HIGH);
+    delayMicroseconds(TIMING_DELAY);
+    digitalWrite(_pin_clock, LOW);
+  }
+  // Serial.println("[end]");
+}
+
+bool SNEStroller::getButtonPressed(int button_requested) {
+  return bitRead(_button_states, button_requested) == 0;
+}
+
+uint16_t SNEStroller::getButtons() {
   return _button_states;
 }
